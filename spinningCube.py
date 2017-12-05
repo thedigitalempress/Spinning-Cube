ae31d5069e58a3edec1b0c3fe33f769e504afd58
thedigitalempress <33044102+thedigitalempress@users.noreply.github.com>
2017-12-05 14:09:33 -0500

    Update spinningCube.py

--- spinningCube.py
+++ spinningCube.py
@@ -28,10 +28,45 @@ edges = (
     (6,7),
     (5,1),
     (5,4),
-    (5,7),
+    (5,7)
 )
 
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,6),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+)
+
+colors = (
+    (1,0,1),
+    (0,1,0),
+    (0,0,1),
+    (1,1,0),
+    (1,1,1),
+    (0,1,1),
+    (1,0,0),
+    (1,1,0),
+    (0,1,1),
+    (1,0,0),
+    (1,1,1),
+    (0,0,1)
+)
+
+
+
 def Cube():
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        x = 0
+        for vertex in surface:
+            x+=1
+            glColor3fv(colors[x])
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
@@ -50,15 +85,17 @@ def main():
 
     glRotatef(0, 0, 0, 0)
 
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
+        glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
-        pygame.time.wait()
+        pygame.time.wait(10)
 
 main()
